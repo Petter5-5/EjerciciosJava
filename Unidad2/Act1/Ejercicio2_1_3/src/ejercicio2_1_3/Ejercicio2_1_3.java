cb3d24576227173a5ee624684b18f554e1b31588
Petter5-5 <yeraytrejosanchez@gmail.com>
2025-10-01 18:00:38 +0200

    2

---
+++ Unidad2/Act1/Ejercicio2_1_3/src/ejercicio2_1_3/Ejercicio2_1_3.java
@@ -0,0 +1,29 @@
+
+package ejercicio2_1_3;
+
+public class Ejercicio2_1_3 {
+
+    public static void main(String[] args) {
+        //1
+        System.out.printf("%-10s %20s%n", "Epic","Epico");
+        //2
+        System.out.printf("%-10s %20s%n", "Play","Jugar");
+        //3
+        System.out.printf("%-10s %20s%n", "Piano","Piano");
+        //4
+        System.out.printf("%-10s %20s%n", "Ball","Pelota");
+        //5
+        System.out.printf("%-10s %20s%n", "Fan","Ventilador");
+        //6
+        System.out.printf("%-10s %20s%n", "Screen","Pantalla");
+        //7
+        System.out.printf("%-10s %20s%n", "Icecream","Helado");
+        //8
+        System.out.printf("%-10s %20s%n", "Helicopter","Helicoptero");
+        //9
+        System.out.printf("%-10s %20s%n", "Duck","Pato");
+        //10
+        System.out.printf("%-10s %20s%n", "Rabbit","Conejo");
+    }
+    
+}
---
+++ Unidad2/Act1/Ejercicio2_1_4/src/ejercicio2_1_4/Ejercicio2_1_4.java
@@ -0,0 +1,11 @@
+
+package ejercicio2_1_4;
+
+public class Ejercicio2_1_4 {
+
+    public static void main(String[] args) {
+        System.out.printf("%-10s %-10s %-10s %-10s %-10s", "Lunes", "Martes", "Mierc.", "Jueves", "Viernes");
+        System.out.println("-----\t");
+    }
+    
+}
