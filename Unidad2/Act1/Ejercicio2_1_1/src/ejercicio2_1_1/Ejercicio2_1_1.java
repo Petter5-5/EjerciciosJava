1d5e9f7c98bd73088d7b95d05c66249a590893ca
Petter5-5 <yeraytrejosanchez@gmail.com>
2025-10-01 17:36:50 +0200

    1

---
+++ Unidad2/Act1/Ejercicio2_1_1/src/ejercicio2_1_1/Ejercicio2_1_1.java
@@ -0,0 +1,10 @@
+
+package ejercicio2_1_1;
+
+public class Ejercicio2_1_1 {
+
+    public static void main(String[] args) {
+        System.out.print("Nombre: Yeray Trejo Sanchez");
+    }
+    
+}
---
+++ Unidad2/Act1/Ejercicio2_1_2/src/ejercicio2_1_2/Ejercicio2_1_2.java
@@ -0,0 +1,12 @@
+
+package ejercicio2_1_2;
+
+public class Ejercicio2_1_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Yeray Trejo Sanchez");
+        System.out.println("Direccion: Trinidad");
+        System.out.println("Telefono: 654 23 43 54");
+    }
+    
+}
